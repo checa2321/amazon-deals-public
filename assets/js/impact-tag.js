8cd6594a1f4dd52f38530f5dbdc8d373c031cf41
checarlos <checa2321@gmail.com>
2026-08-18 22:12:30 -0500

    Add Impact.com (Walmart affiliate) publisher tracking tag

---
+++ assets/js/impact-tag.js
@@ -0,0 +1,3 @@
+(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7589183-b737-4798-b512-560aa7121b471.js','script','impactStat',document,window);
+impactStat('transformLinks');
+impactStat('trackImpression');
